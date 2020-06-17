b1db07cf784c6a4eb5b0c8d561c712fc6033ed65
tung <tung.nguyen271193@gmail.com>
2020-06-17 23:45:15 +0300

    api

--- src/App.js
+++ src/App.js
@@ -31,7 +31,12 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+
   }
 
   onInputChange = (event) => {
@@ -44,14 +49,8 @@ class App extends Component {
       .predict(
         Clarifai.FACE_DETECT_MODEL, 
         this.state.input)
-      .then(
-        function(response) {
-          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        },
-        function(err) {
-          // there was an error
-        }
-      );
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err));
   }
 
   render() {
