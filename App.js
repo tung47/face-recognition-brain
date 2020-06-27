185ca9447cd9e17f3da5e5390da8aaeff34e0d47
tung <tung.nguyen271193@gmail.com>
2020-06-27 16:27:05 +0300

    modify

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Clarifai from 'clarifai';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
@@ -33,30 +34,30 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFace = data.output[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - clarifaiFace.right_col * width,
-      bottomRow: height - clarifaiFace.bottom_row * height
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
   }
 
   displayFaceBox = (box) => {
-    console.log(box);
     this.setState({box: box});
   }
 
   onInputChange = (event) => {
-    this.setState({ input: event.target.value});
+    this.setState({input: event.target.value});
   }
 
   onButtonSubmit = () => {
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,12 +5,12 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magin Brain will detect faces in your pictures. Git it a try.'}
+        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
-          <button 
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <button
             className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
             onClick={onButtonSubmit}
           >Detect</button>
