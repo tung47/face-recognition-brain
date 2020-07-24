f822036d1df10ca6a362cfe02416da72488bc057
tung <tung.nguyen271193@gmail.com>
2020-07-24 22:58:44 +0300

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
